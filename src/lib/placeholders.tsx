bf9c50c2832b4d7a53de0c194a7b8dfc80dfee40
mikhaleff8230 <sale@sancan.ru>
2026-06-12 12:42:31 +0300

    Restore shared placeholders helper

---
+++ src/lib/placeholders.tsx
@@ -0,0 +1,6 @@
+import productPlaceholder from '@/assets/images/placeholders/product.svg';
+import avatarPlaceholder from '@/assets/images/placeholders/avatar.svg';
+import userPlaceholder from '@/assets/images/placeholders/user.svg';
+
+export { avatarPlaceholder, productPlaceholder, userPlaceholder };
+
